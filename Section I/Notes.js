62caa7ff12f757f0f9cb6a864c71d027e00d7e93
Ryan Kim <ryankrdh@gmail.com>
2021-08-26 23:24:06 -0700

    notes

--- Section I/Notes.js
+++ Section I/Notes.js
@@ -4,3 +4,23 @@
 After every unit and subunit, summarize in 2-3 sentence what you have learned.
 Paraphrase new terms, concepts, and procecures.
 */
+
+/* 
+Call Agenda:
+
+✔ Identify your career goals and review your Career Strategy Plan
+
+✔ Develop a job search strategy that will help you reach important milestones as you progress through this course
+
+✔ Learn about the career coaching curriculum and coming coaching calls 
+
+Career Strategy Plan Link:
+https://docs.google.com/document/d/1eygvKotStJ8NOVc6U-Cm31VQCASD2Du4hjk9y6oRsPs/edit?usp=sharing
+*/
+
+/* 
+******** QUESTIONS ********
+
+Avoiding plagiarism.
+
+*/
