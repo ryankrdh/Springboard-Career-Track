518621314521583332efcad29bf4c91e5d567bb4
Ryan Kim <ryankrdh@gmail.com>
2021-08-31 21:28:47 -0700

    notes

--- Section I/Notes.js
+++ Section I/Notes.js
@@ -24,3 +24,20 @@ https://docs.google.com/document/d/1eygvKotStJ8NOVc6U-Cm31VQCASD2Du4hjk9y6oRsPs/
 Avoiding plagiarism.
 
 */
+
+/*
+
+HTML refresher
+
+<label for="username">enter Username</label>
+<input type="text" id="username">
+
+# The above label needs a username after the for because the input type id is username and it is used to bound the label to a form field.
+
+# # Understand the most common input tag attributes:
+* Type: Type of form control
+* Name: Name of the form control. Submitted with the form as part of a name/value pair.
+* Placeholder: Text that appears in the form control when it has no value set.
+* Value: The initial value of the control.
+
+*/
