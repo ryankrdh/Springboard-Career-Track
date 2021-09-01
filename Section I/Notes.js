4d4b58cacc6b7e04e119a24cbddb4c77d02476c1
Ryan Kim <ryankrdh@gmail.com>
2021-08-31 23:30:13 -0700

    notes

--- Section I/Notes.js
+++ Section I/Notes.js
@@ -30,7 +30,8 @@ Avoiding plagiarism.
 HTML refresher
 
 <label for="username">enter Username</label>
-<input type="text" id="username">
+<input type="text" name="name" placeholder=""/>
+<input type="submit" value="Submit your username"/>>
 
 # The above label needs a username after the for because the input type id is username and it is used to bound the label to a form field.
 
@@ -39,5 +40,92 @@ HTML refresher
 * Name: Name of the form control. Submitted with the form as part of a name/value pair.
 * Placeholder: Text that appears in the form control when it has no value set.
 * Value: The initial value of the control.
+
+You can only checkone button at a time if you have two radio buttons with the same name.
+
+
+CSS refresher
+
+specificity is important
+ID > class > element selector
+
+Basic CSS Properties
+
+div {
+    background: crimson;
+    color: grey;
+    font-family: Heletica;
+    font-size: 18px;
+    cursor: pointer;
+}
+
+font-size: rem refers to the property's inital value.
+
+div {
+    width: 10%;
+    height: 20%;
+    margin: 10px;
+    padding: 20px;
+    border-radius: 10px;
+}
+
+* Know shorthanded css properties
+
+* Know CSS Positioning: static, fixed, relative absolute.
+*/
+
+/*
+## JAVASCRIPT MUST KNOWS
+
+This goes in the body.
+<script>src="app.js"</script>
+
+
+KEYWORD    |     CAN REASSIGN     |     CAN REDECLARE     |     SCOPE RULES
+           |                      |                       |
+var        |        YES           |         YES           |      FUNCTION
+let        |        YES           |         NO            |     BLOCK
+const      |        NO            |         NO            |     BLOCK
+
+
+
+GLOBAL SCOPE: The area outside all the functions is consider the global scope and the variables defined inside the global scope can be accessed and altered in any other scopes.
+
+LOCAL SCOPE: Variables declared inside the functions become Local to the function and are considered in the corresponding local scope. 
+
+FUNCTION SCOPE: Whenever you declare a variable in a function, the variable is visible only within the function. You can't access it outside the function. var is the keyword to define variable for a function-scope accessibility.
+
+BLOCK SCOPE: A block scope is the area within if, switch conditions or for and while loops. Generally speaking, whenever you see {curly brackets}, it is a block. In ES6, const and let keywords allow developers to declare variables in the block scope, which means those variables exist only within the corresponding block.
+
+LEXICAL SCOPE: Lexical Scope
+Another point to mention is the lexical scope. Lexical scope means the children scope have the access to the variables defined in the parent scope. The children functions are lexically bound to the execution context of their parents.
+
+
+The following values are always falsy:
+
+false
+0 (zero)
+-0 (minus zero)
+0n (BigInt zero)
+'', "", `` (empty string)
+null
+undefined
+NaN
+
+
+Everything else is truthy. That includes:
+
+'0' (a string containing a single zero)
+'false' (a string containing the text “false”)
+[] (an empty array)
+{} (an empty object)
+function(){} (an “empty” function)
+
+
+
+
+
+
+
 
 */
