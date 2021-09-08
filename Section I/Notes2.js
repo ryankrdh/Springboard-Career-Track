79b3c288938a559371cb7782361cc4d15c9ec814
Ryan Kim <ryankrdh@gmail.com>
2021-09-07 23:17:03 -0700

    notes

--- Section I/Notes2.js
+++ Section I/Notes2.js
@@ -122,8 +122,29 @@ range.value = 5
 Manipulating classes:
 * setAttribute('class') - this will override the class
 * className - This will give you a string representation of the class.
-* classList - This will give you an array-like object to add, remove, or toggle classes.
+* classList(useful) - This will give you an array-like object to add, remove, or toggle classes.
 
 classList methods: add, remove, toggle, contains
 
+
+function toggleAllTodos() {
+    const todos = document.querySelectorAll('li');
+
+    for (let li of todos) {
+        li.classList.toggle('completed');  
+    }
+}
+
+
+const h1 = document.querySelector('h1');
+
+setInterval(function () {
+    if(h1.classList.contains('big')) {
+        h1.innerText = "SAD";
+    } else {
+        h1.innerText = "HAPPY"
+    }
+    h1.classList.toggle('big');
+    h1.classList.toggle('small');
+}, 1000)
 */
