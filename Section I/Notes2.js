e7342513b173b72cad68fe49fa1aca22eee87292
Ryan Kim <ryankrdh@gmail.com>
2021-09-13 21:32:46 -0700

    notes

--- Section I/Notes2.js
+++ Section I/Notes2.js
@@ -409,4 +409,13 @@ form.addEventListener('submit', function (e) {
   input.value = '';
 });
 
+
+// data attribute
+
+const ul = document.querySelector('#cars');
+ul.addEventListener('click', function(e) {
+    console.log(e.target.dataset.year);
+    e.target.dataset.sold = 'true';
+});
+
 */
