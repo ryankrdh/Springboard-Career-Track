1b1a8c3cff5485e169da630d1162d0824aefc239
Ryan Kim <ryankrdh@gmail.com>
2021-09-19 19:26:23 -0700

    notes git commit -m notes

--- Section I/Developer Fundamentals.js
+++ Section I/Developer Fundamentals.js
@@ -2,6 +2,15 @@
 
 /*
 
+TERMINAL SHORTCUTS:
+
+cd [name]: to jump into the named directory. You can use absolute path or relative path. 
+Example of Absolute: cd/users/recordingusers
+Example of Relative: 
+cd ..: go to previous directory.
+ls: see the current files and folders in the directory.
+pwd: shows the absolute path.
+
 
 
 */
