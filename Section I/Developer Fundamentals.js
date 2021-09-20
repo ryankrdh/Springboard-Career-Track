852daf0af048cef901475298a876ee9fd67f46eb
Ryan Kim <ryankrdh@gmail.com>
2021-09-19 20:26:42 -0700

    notes

--- Section I/Developer Fundamentals.js
+++ Section I/Developer Fundamentals.js
@@ -9,10 +9,28 @@ cd [name]: to jump into the named directory. You can use absolute path or relati
 Example of Absolute: cd/users/recordingusers/stuff
 Example of Relative: cd stuff/
 
+referencing home directory: cd ~/stuff/topsecret/
 
 cd ..: go to previous directory.
 ls: see the current files and folders in the directory.
 pwd: shows the absolute path.
+ls [name]/: to look inside without going in
+ls -a: list all. (- is a flag)
+ls -l: lists more information regarding the files.
+
+man: will give us the manual.
+
+*if there is a space, use quotes*
+
+Create files and folders in Terminal using mkdir and touch.
+Move files and folders in Terminal using mv.
+Copy files and folders in Terminal using cp.
+Remove files and folders in Terminal using rm and rmdir.
+cat app.js: prints out the contents
+echo "word": it will repeat the words back. 
+echo ".env" > .gitignore: replaces all the content in there. 
+
+less, open
 
 
 
