0adf1b20c0bd1109e2476169cabcec50dca0bb04
Ryan Kim <ryankrdh@gmail.com>
2021-09-19 19:27:53 -0700

    notes

--- Section I/Developer Fundamentals.js
+++ Section I/Developer Fundamentals.js
@@ -4,9 +4,12 @@
 
 TERMINAL SHORTCUTS:
 
-cd [name]: to jump into the named directory. You can use absolute path or relative path. 
-Example of Absolute: cd/users/recordingusers
-Example of Relative: 
+cd [name]: to jump into the named directory. You can use absolute path or relative path. it needs to be relative to where you are. You can go anywere using absolute.
+
+Example of Absolute: cd/users/recordingusers/stuff
+Example of Relative: cd stuff/
+
+
 cd ..: go to previous directory.
 ls: see the current files and folders in the directory.
 pwd: shows the absolute path.
