7550d685b8ffc64e07f4373435c0e910bffe5776
Ryan Kim <ryankrdh@gmail.com>
2021-09-21 23:27:39 -0700

    notes

--- Section I/Developer Fundamentals.js
+++ Section I/Developer Fundamentals.js
@@ -117,6 +117,9 @@ UNIT TESTING
 */
 
 function calculateTaxes(income) {
+  if (!Number.isFinite(income)) {
+    throw new Error('Invalid income!');
+  }
   if (income > 30000) {
     return income * 0.25;
   } else {
--- Section I/Developer Fundamentals.test.js
+++ Section I/Developer Fundamentals.test.js
@@ -1,3 +1,18 @@
 it('should calculate the high tax bracket', function () {
   expect(calculateTaxes(50000)).toEqual(12500);
+  expect(calculateTaxes(100000)).toEqual(25000);
+});
+
+it('should calculate the low tax bracket', function () {
+  expect(calculateTaxes(10000)).toEqual(1500);
+  expect(calculateTaxes(1000)).toEqual(150);
+  expect(calculateTaxes(0)).toEqual(0);
+});
+
+// To use matchers like toThrowError, the function needs to be wrapped in parenthesis.
+
+it('should reject invalid incomes', function () {
+  expect(() => calculateTaxes('asdfasdf')).toThrowError();
+  expect(() => calculateTaxes([])).toThrowError();
+  expect(() => calculateTaxes(true)).toThrowError();
 });
