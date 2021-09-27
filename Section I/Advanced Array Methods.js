e9e63c1e64be17d7a365d27237fdcd6a4d605f2f
Ryan Kim <ryankrdh@gmail.com>
2021-09-26 21:32:52 -0700

    notes

---
+++ Section I/Advanced Array Methods.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/* 
+CALLBACKS
+
+function add (x,y) {}
+function subtract () {}
+function multiply () {}
+function divide () {}
+
+const mathFuncs = [add, subract, multiply, divide]
+
+mathFuncs[3](4,5)
+
+
+*/
