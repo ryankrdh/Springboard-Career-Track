e7700d812db781f1b364aa8058761cd6eff309b9
Ryan Kim <ryankrdh@gmail.com>
2021-09-27 17:51:51 -0700

    notes

--- Section I/Advanced Array Methods.js
+++ Section I/Advanced Array Methods.js
@@ -13,4 +13,15 @@ const mathFuncs = [add, subract, multiply, divide]
 mathFuncs[3](4,5)
 
 
+forEach:
+
+* Loops through an array.
+* Runs a callback function for each value in the array and then returns undefined.
+* forEach will always return undefined - no matter what.
+
+const colors = [ 'teat', 'cyan', 'peach', 'purple'];
+
+colors.forEach(function() {
+    
+})
 */
