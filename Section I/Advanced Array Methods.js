250cd81ffe366481ba9d9a8e8217c3d642ca5656
Ryan Kim <ryankrdh@gmail.com>
2021-09-27 19:17:34 -0700

    notes

--- Section I/Advanced Array Methods.js
+++ Section I/Advanced Array Methods.js
@@ -19,9 +19,37 @@ forEach:
 * Runs a callback function for each value in the array and then returns undefined.
 * forEach will always return undefined - no matter what.
 
-const colors = [ 'teat', 'cyan', 'peach', 'purple'];
 
-colors.forEach(function() {
-    
-})
+
 */
+
+const colors = ['teal', 'cyan', 'peach', 'purple'];
+
+// the arr will result in entire arrays and is rarely used.
+colors.forEach(function (val, i, arr) {
+  const caps = val.toUpperCase();
+  console.log(`At index ${i}, ${caps}`);
+  console.log(arr);
+});
+
+// for Each ----------------------------------------------------------
+
+const prices = [30.99, 19.99, 2.5, 99.0];
+let total = 0;
+prices.forEach(function (price) {
+  total += price;
+});
+console.log(total);
+
+// for of ------------------------------------------------
+
+const prices = [30.99, 19.99, 2.5, 99.0];
+let total = 0;
+// for loop: for(let i = 0; i < prices.length; i ++)
+for (let price of prices) {
+  total += price;
+}
+console.log(total);
+// DIFFERENCE BETWEEN FOR OF, FOR LOOP, FOR EACH:
+// for of is a newer concept and for each has been around for a long time.
+// Did not have for of back then. forEach came before for of
