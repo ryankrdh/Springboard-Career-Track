9030e61dd5ef1aca7ca83633673a7921ee122ed8
Ryan Kim <ryankrdh@gmail.com>
2021-10-06 23:33:04 -0700

    notes

--- Section I/elevator-pitch.js
+++ Section I/elevator-pitch.js
@@ -23,7 +23,15 @@
 /*
 // ELEVATOR PITCH
 
-Hi my name is Ryan and I'll be graduating from Springboard in June of 2022.
+Hi! Nice to meet you! 
 
-I am looking for a software engineer position in Seattle. 
+My name is Ryan and I'll be graduating from Springboard in June of 2022.
+
+I am looking for a software engineering position in Seattle and I'm particularly interested in working for startup-like environments where I can grow fast. 
+
+I have experience in programming full-stack applications, working mainly on back-end. 
+
+I am very team-driven and passionate, and I'm sure I'll be a great addition to your team.
+
+Would you be interested in connecting with me to discuss any opportunities?
 */
