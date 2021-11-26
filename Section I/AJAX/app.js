2258df8d06e1c8475e643a0c809fa3626c793332
ryankrdh <ryankrdh@gmail.com>
2021-11-25 22:25:01 -0800

    Notes

---
+++ Section I/AJAX/app.js
@@ -0,0 +1,61 @@
+// async function getData() {
+//   const response = axios.get('https://swapi.co/api/planets/');
+//   const { next, results } = response.data;
+//   console.log(next);
+//   for (let planet of results) {
+//     console.log(planet.name);
+//   }
+//   const response2 = await axios.get(next);
+//   //   const { results } = response.data;
+//   const results2 = response2.data.results);
+//   for (let planet of results) {
+//     console.log(planet.name);
+//   }
+// }
+
+// getData();
+// console.log('I happen after getData()');
+
+// ROCKET EXAMPLE --------------------------------------
+
+async function getLaunches() {
+  const res = await axios.get(
+    'https://api.spacexdata.com/v3/launches/upcoming'
+  );
+  renderLaunches(res.data);
+  //   console.log(res.data);
+}
+
+function renderLaunches(launches) {
+  const ul = document.querySelector('#launches');
+  for (let launch of launches) {
+    ul.append(makeLaunchLI(launch));
+    // console.log(launch.mission_name);
+    // console.log(launch.rocket.rocket_name);
+  }
+}
+
+function makeLaunchLI(launch) {
+  const newLI = document.createElement('LI');
+  const mission = document.createElement('B');
+  // newLI.innerText = launch.mission_name;
+  mission.innerText = launch.mission_name;
+  newLI.append(mission);
+  newLI.innerHTML += ` - ${launch.rocket.rocket_name}`;
+  return newLI;
+}
+
+document.querySelector;
+
+// DOG EXAMPLE --------------------------------------
+
+/*
+get the random categories first
+
+state: the events that will happen. 
+default states: ?
+
+looping to create the table. this can determine the state of the box.
+
+transform the data 
+*/
--- Section I/AJAX/notes.md
+++ Section I/AJAX/notes.md
@@ -30,3 +30,20 @@ AJAX is a technique in Javascript for sending requests and receiving responses f
 What Does AJAX stand for?
 
 AJAX originally was an acronym for “Asynchronous Javascript and XML”. However many people don’t send XML over AJAX nowadays; it’s more common to send HTML or JSON. The technology is still the same, though, even if the data payload is commonly different. Ultimately, AJAX is a cooler sounding acronym than AJAJ or AJAH.
+
+Why Use AJAX?
+Don’t need to reload entire page if just 1 thing is changing
+Interactive web sites
+Fewer full page loads from server
+Your JS can talk to other servers directly
+Less info has to go across network
+AJAX with Axios
+You don’t have to use Axios for this
+
+There is an old, clunky built-in tool: (XMLHttpRequest)
+
+Or a newer-but-still-clunky built-in tool: (fetch)
+
+Or lots of other libraries (including jQuery)
+
+… but we’ll use axios for now! It’s featureful & popular
