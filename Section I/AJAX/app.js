bee65b2c65ff5db921d9d6eba7df2c9d48461d23
ryankrdh <ryankrdh@gmail.com>
2021-11-28 18:13:17 -0800

    Notes

--- Section I/AJAX/app.js
+++ Section I/AJAX/app.js
@@ -63,5 +63,17 @@ transform the data
 // AXIOS POST EXAMPLE ---------------------------------
 
 async function getUsers() {
-  const res = await axios.get('https://reqres.in');
+  const res = await axios.get('https://reqres.in/api/users');
+  console.log(res);
 }
+
+async function createUser() {
+  const res = await axios.post('https://reqres.in/api/users', {
+    username: 'ButtersTheChicken',
+    email: 'butters@gmail.com',
+    age: 1,
+  });
+  console.log(res);
+}
+// getUsers();
+createUser();
