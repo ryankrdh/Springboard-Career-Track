5cf7a96e7b12412ce8729740db6b2551f50efd1e
ryankrdh <ryankrdh@gmail.com>
2022-01-03 23:04:55 -0800

    notes

---
+++ Section II/flask_introduction.py
@@ -0,0 +1,19 @@
+# Goals
+# Describe the purpose and responsibilities of a web framework
+# Build small web applications using Python and Flask
+# Set environmental variables for local Flask development
+# Handle GET and POST requests with Flask
+# Extract data from different parts of the URL with Flask
+# Web Frameworks
+# A Quick Demo
+# (venv) $ FLASK_ENV=development flask run
+# * Environment: development
+# * Debug mode: on
+# * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+# * Restarting with stat
+# * Debugger is active!
+# * Debugger PIN: 160-080-703
+# What is a Web Server?
+# A program that’s running on a machine and waiting for a web request.
+
+# Note: A web server is a technology that can process requests and issue responses via HTTP, a protocol used to distribute information on the world wide web. Though it’s also used to refer to computer systems and even internet “appliances,” we’ll use web server to refer to the software running on a machine that’s waiting to respond to HTTP requests.
