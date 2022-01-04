aabfca6ce60c2ef564cd17905b375bbb4ad99f50
ryankrdh <ryankrdh@gmail.com>
2022-01-03 23:05:16 -0800

    notes

--- Section II/flask_introduction.py
+++ Section II/flask_introduction.py
@@ -17,3 +17,43 @@
 # A program that’s running on a machine and waiting for a web request.
 
 # Note: A web server is a technology that can process requests and issue responses via HTTP, a protocol used to distribute information on the world wide web. Though it’s also used to refer to computer systems and even internet “appliances,” we’ll use web server to refer to the software running on a machine that’s waiting to respond to HTTP requests.
+
+# Web Application
+# The ability to start a server in “listening” for requests, and then issue responses:
+
+# GET /hello     (http://server/hello)
+# ↓
+
+# <html>
+#   <body>
+#     <h1>Hello World!</h1>
+#   </body>
+# </html>
+# Note: To keep code samples short in the presentation, we’re eliding some less-important HTML markup. The shortest valid HTML skeleton in modern HTML would actually be:
+
+# <!doctype html>
+# <html>
+#   <head>
+#     <title>Hello</title>
+#   </head>
+#   <body>
+#     <h1>Hello World!</h1>
+#   <body>
+# </html>
+# Flask is a Web Framework
+# Set of functions, classes, etc. that help you define
+# Which requests to respond to
+# http://server/about-us
+# http://server/post/1
+# How to respond to requests
+# Shows an “About Us” page
+# Show the 1st blog post
+# Like a library, but bigger and more opinionated
+# Usage is similar to the Python Standard Library.
+# Using the Python Standard Library
+
+# from random import choice, randint
+ 
+# Using Flask
+
+# from flask import Flask, request
