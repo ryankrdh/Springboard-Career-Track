9e9d79532dc93462695cb6151c47573c11b5fa9f
ryankrdh <ryankrdh@gmail.com>
2022-01-05 22:39:30 -0800

    notes

--- Section II/flask_introduction.py
+++ Section II/flask_introduction.py
@@ -110,3 +110,31 @@
 # (venv) $ export FLASK_ENV=development
 # Add that line to shell config to run on every new terminal session.
 
+# Adding Routes
+# Making Responses
+# A function that returns web response is called a view
+
+# Response is a string
+# Usually, a string of HTML
+# So, our function returns an HTML string:
+
+# @app.route('/hello')
+# def say_hello():
+#     """Return simple "Hello" Greeting."""
+
+#     html = "<html><body><h1>Hello</h1></body></html>"
+#     return html
+# Handling Requests
+# On requesting http://localhost:5000/hello in browser, function is called:
+
+# @app.route('/hello')
+# def say_hello():
+#     """Return simple "Hello" Greeting."""
+
+#     html = "<html><body><h1>Hello</h1></body></html>"
+#     return html
+# Flask lets you “route” a URL to a function
+# @app.route('/hello') is a Python “decorator”
+# “/hello” in the decorator maps directly to the URL the user requested
+# Now we can get to this at http://localhost:5000/hello
+
