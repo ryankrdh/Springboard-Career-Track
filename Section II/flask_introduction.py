e5f4c4e2dd756e6a9b103eb9a106b46a4fee3543
ryankrdh <ryankrdh@gmail.com>
2022-01-05 22:39:53 -0800

    notes

--- Section II/flask_introduction.py
+++ Section II/flask_introduction.py
@@ -138,3 +138,25 @@
 # “/hello” in the decorator maps directly to the URL the user requested
 # Now we can get to this at http://localhost:5000/hello
 
+
+
+# Serving at the Root
+# @app.route('/')
+# def index():
+#     """Show homepage"""
+
+#     return """
+#       <html>
+#         <body>
+#           <h1>I am the landing page</h1>
+#         </body>
+#       </html>
+#       """
+
+# This function will get called if the user requests http://localhost:5000/.
+
+# Now we can reach this page at http://localhost:5000
+
+# What Routes Return
+# Routes should return strings!
+
