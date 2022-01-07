437db9dad80eca289f84b7e2a8329f66273a6e92
ryankrdh <ryankrdh@gmail.com>
2022-01-06 19:17:54 -0800

    notes

--- Section II/flask_introduction.py
+++ Section II/flask_introduction.py
@@ -176,3 +176,33 @@
 #     return f"<h1>Searching for {term}</h1>"
 # request.args is a dict-like object of query parameters.
 
+# Handling POST Requests
+# By default, a route only responds to GET requests
+
+# To accept POST requests, must specify that:
+
+# @app.route("/my/route", methods=["POST"])
+# def handle_post_to_my_route():
+#    ...
+# Example POST Request
+# @app.route("/add-comment")
+# def add_comment_form():
+#     """Show form for adding a comment."""
+
+#     return """
+#       <form method="POST">
+#         <input name="comment">
+#         <button>Submit</button>
+#       </form>
+#       """
+
+ 
+# @app.route("/add-comment", methods=["POST"])
+# def add_comment():
+#     """Handle adding comment."""
+
+#     comment = request.form["comment"]
+
+#     # TODO: save that into a database!
+
+#     return f'<h1>Received "{comment}".</h1>'
