24bd3a29c196b54c78263982437b42a7533bbdd1
ryankrdh <ryankrdh@gmail.com>
2022-01-15 22:24:47 -0800

    notes

---
+++ Section II/flask_notes/flask_tools.py
@@ -0,0 +1,26 @@
+# Goals
+# Explore other common features of web frameworks like Flask, including:
+# Redirecting
+# Flash messaging
+# Returning JSON data
+# Debug Flask errors from the error page
+# Set break points in Python code with pdb
+# Redirecting
+# What is an HTTP redirect?
+
+# An HTTP response
+# The status code is a “redirect code” (often, 302)
+# It contains a URL for browser to re-request
+# Typically, for ancient browsers, contains HTML with a link
+# $ curl -v http://localhost:5000/redirect-me
+
+# < HTTP/1.0 302 FOUND
+# < Content-Type: text/html; charset=utf-8
+# < Location: http://localhost:5002/somewhere-else
+
+# <h1>Redirecting...</h1>
+# <p>You should be redirected automatically to target URL:
+#  <a href="/somewhere-else">/somewhere-else</a>.
+#  If not click the link.</p>
+# Your browser won’t typically show you this page — it makes the re-request so fast you don’t even notice it happened!
+
