a13d1b0ce63e311a1283c642eadae2acb762f9c2
ryankrdh <ryankrdh@gmail.com>
2022-01-17 23:04:57 -0800

    notes

--- Section I/elevator-pitch.js
+++ Section I/elevator-pitch.js
@@ -90,3 +90,110 @@ automate hard processes focusing on daily life activities.
 
 
 */
+
+/* 
+I'm not good at estimating how long a project will take me.
+I tend to derail when I learn things. curious about everything.
+
+
+What work environment do you dislike working in?
+I tend to not do well in a solo setting. I believe that true efficiency can only be achieve through a team where you share knowledge and cover each others weaknesses
+
+
+It is often said that a career in healthcare is a 'thankless job.' How can we keep you motivated and engaged, even on the days when you feel your work goes unnoticed?
+I get mainly get motivated by growth. As long as I'm learning and improving, I get satisfaction from that. Like simple as solving an algorithm problem.
+
+
+Patient care requires a strong amount of compassion. Do you consider yourself a compassionate person?
+I work in a dental office that specializes in dental anxiety. Compassion is almost a requirement to work there in order to tend to the patients needs. I've always been responsive to my patients' needs and always empathetic
+
+
+When have you shown a willingness to learn a new method or new approach to solving a problem?
+
+
+Accolade seeks to hire those with strong problem solving skills. When were you able to successfully resolve a problem in the workplace?
+
+
+In your opinion, what makes you a great problem solver?
+
+
+How would you describe your personality?
+
+
+Think about a difficult boss, professor or coworker. What made him or her difficult? How did you successfully interact with this person?
+
+
+In order to work for Accolade, you must be able to pass a full criminal background check. Do you consent to a full background check?
+
+
+What part of your healthcare career brings you the most stress?
+
+
+Tell me about your healthcare related education and training.
+
+
+Accolade has a diverse workforce. When have you worked among a diverse group of people?
+
+
+At Accolade we take pride in our great relationships with clients, vendors, coworkers, and patients. Do you consider yourself to be a strong relationship builder?
+
+
+In the healthcare services industry there are many emotions in a day. Have your emotions ever been in the way of your productivity?
+
+
+At Accolade we seek to hire individuals who display a keen interest in the healthcare arena. Would you consider yourself dedicated to a career in healthcare services?
+
+
+What is your patient care philosophy?
+
+
+Healthcare service companies require strong organizational skills and attention to detail. How do you ensure that your work is properly organized, and highly accurate?
+
+
+If you could expand your knowledge and expertise in any area of healthcare services, which would you choose?
+
+
+Have you ever been involved in ordering medical supplies, maintaining inventory, or other types of healthcare-related administrative duties?
+
+
+At Accolade we seek to hire individuals who have ambitions of growing their career. Where do you see yourself in 3-5 years?
+
+
+Do you prefer to work in a team based position or individually?
+
+
+Accolade supports a healthy work/life balance for all employees. How do you balance life and work?
+
+
+If you could expand your knowledge and expertise in any healthcare service area, which would you choose?
+
+
+If Accolade hired you today, what would you accomplish first?
+
+
+With the ongoing changes in the healthcare services industry, how do you keep your knowledge current?
+
+
+Rate your problem solving skills from 1-10. How do you justify your rating?
+
+
+What is your greatest weakness?
+
+
+Why do you think you will be successful in this role with Accolade?
+
+
+Accolade was initially looking for someone with 5 years' experience in a similar role. Considering you have just 2 years' experience, would you be willing to accept this position at a lower salary?
+
+
+Talk about a time when you were in a team situation everyone was not on the same page.
+
+
+How would you brand yourself?
+
+
+What is a situation where you had to do something you didn't want to do but had to out of obligation and what did you do?
+
+
+
+*/
