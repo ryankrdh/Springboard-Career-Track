051ec6250e517bb8a223db2ddcdb926f76b3d01a
ryankrdh <ryankrdh@gmail.com>
2022-01-22 22:57:00 -0800

    notes

--- Section II/flask-session-demo/app.py
+++ Section II/flask-session-demo/app.py
@@ -164,3 +164,27 @@ def show_secret_invite():
         return render_template("invite.html")
     else:
         return redirect("/login-form")
+
+
+
+
+
+# ------------------------------------------------------------------------------
+
+# Motivation
+# Saving “State”
+# HTTP is what’s called a “stateless” protocol.
+
+# On its own, it remembers nothing.
+
+# It’s like a goldfish. Every time it circles around, what it sees is brand new.
+
+# Some Ways To Save State
+# Passing info in a query param / POST form hidden field
+# /step-zero?fav-color=blue → /step-one?fav-color=blue → …
+# Keeping info in URL path
+# /fav-color/blue/step-zero → /fav-color/blue/step-one → …
+# Using JS localStorage API
+# Nice, but only JS can access this — you can’t get data on server
+# Useful for single-page applications or heavily AJAX-driven apps
+# Using cookies / sessions
