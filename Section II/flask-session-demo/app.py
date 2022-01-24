28a3df82563179f4effb2f818e6dce568167fe19
ryankrdh <ryankrdh@gmail.com>
2022-01-23 17:33:07 -0800

    notes

--- Section II/flask-session-demo/app.py
+++ Section II/flask-session-demo/app.py
@@ -188,3 +188,30 @@ def show_secret_invite():
 # Nice, but only JS can access this — you can’t get data on server
 # Useful for single-page applications or heavily AJAX-driven apps
 # Using cookies / sessions
+
+# Cookies
+# Flask’s session is powered by cookies; let’s start there
+
+# Cookies Save “State”
+# Cookies are a way to store small bits of info on client (browser)
+
+# What is a Cookie?
+# Cookies are name/string-value pair stored by the client (browser).
+
+# The server tells client to store these.
+
+# The client sends cookies to the server with each request.
+
+
+# Site	Cookie Name	Value
+# rithmschool.com	number_visits	“10”
+# rithmschool.com	customer_type	“Enterprise”
+# localhost:5000	favorite_food	“taco”
+# Cookies, A Conversation
+# Browser: I’d like to get the resource /upcoming-events.
+# Server: Here’s some HTML. Also, please remember this piece of information: favorite_food is "taco".
+# Browser (stores this somewhere on the computer)
+# Browser: I’d like to get the resource /event-detail. Also, you told me to remind you that favorite_food is "taco".
+# Server: Here’s the HTML for that.
+# Browser: I’d like to get the resource /calendar.jpg. Also, you told me to remind you that favorite_food is "taco".
+# …
