951b1d92e9bc8e4e8e71c2671e1af0bfccc7e34a
ryankrdh <ryankrdh@gmail.com>
2022-01-25 17:18:36 -0800

    notes

--- Section II/flask-session-demo/app.py
+++ Section II/flask-session-demo/app.py
@@ -261,3 +261,16 @@ def show_secret_invite():
 # Send only to books.site.com or everything at site.com?
 # HttpOnly - HTTP-only cookies aren’t accessible via any kind of JavaScript
 # Useful for cookies that contain server-side information and don’t need to be available to JavaScript.
+
+# Comparison of Types of Browser Storage
+# LocalStorage
+# Stores data with no expiration date, and gets cleared only through JavaScript, or clearing the Browser cache
+# Domain specific
+# Storage limit is much larger than a cookie.
+# SessionStorage
+# Stores data only for until the browser or tab is closed.
+# Storage limit is much larger than a cookie.
+# Cookie
+# Cookies can be made secure by setting the httpOnly flag as true for that cookie. This prevents client-side access to that cookie
+# Sent from the browser to the server for every request to the same domain
+# Set usually from server-side. Can we read by a server
