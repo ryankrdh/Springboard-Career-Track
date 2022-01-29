65f29cf51b7de0cca9fd743de8d7b54408fa742a
ryankrdh <ryankrdh@gmail.com>
2022-01-28 23:42:59 -0800

    notes

--- Section II/flask-session-demo/app.py
+++ Section II/flask-session-demo/app.py
@@ -318,3 +318,31 @@ def show_secret_invite():
 # Send a cookie with “session key”, which tells server how to get the real data
 # Useful when you have lots of session data, or for complex setups
 # Flask can do this with the add-on Flask-Session
+
+
+# from flask import Flask
+# from datetime import datetime
+# import re
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def home():
+#     return "Hello, Flask!"
+
+# @app.route("/hello/<name>")
+# def hello_there(name):
+#     now = datetime.now()
+#     formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+#     # Filter the name argument to letters only using regular expressions. URL arguments
+#     # can contain arbitrary text, so we restrict to safe characters only.
+#     match_object = re.match("[a-zA-Z]+", name)
+
+#     if match_object:
+#         clean_name = match_object.group(0)
+#     else:
+#         clean_name = "Friend"
+
+#     content = "Hello there, " + clean_name + "! It's " + formatted_now
+#     return content
