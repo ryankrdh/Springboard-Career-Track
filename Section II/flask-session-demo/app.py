0e138d0be45c8e60721d1c10bf6a0de3b6bfd8f3
ryankrdh <ryankrdh@gmail.com>
2022-01-29 22:39:37 -0800

    notes

--- Section II/flask-session-demo/app.py
+++ Section II/flask-session-demo/app.py
@@ -341,3 +341,10 @@ def show_secret_invite():
 
 #     content = "Hello there, " + clean_name + "! It's " + formatted_now
 #     return content
+
+# Hooking in WSGI Middleware
+# To add WSGI middleware to your Flask application, wrap the application’s wsgi_app attribute. For example, to apply Werkzeug’s ProxyFix middleware for running behind Nginx:
+
+# from werkzeug.middleware.proxy_fix import ProxyFix
+# app.wsgi_app = ProxyFix(app.wsgi_app)
+# Wrapping app.wsgi_app instead of app means that app still points at your Flask application, not at the middleware, so you can continue to use and configure app directly.
