7d4e8e60c90708d4a038984eccc3d98b2b366f7c
ryankrdh <ryankrdh@gmail.com>
2022-02-16 21:52:50 -0800

    notes

--- Section II/flask-testing-demo/test.py
+++ Section II/flask-testing-demo/test.py
@@ -13,3 +13,11 @@ class ColorsViewsTestCase(TestCase):
             
             self.assertEqual(res.status_code, 200)
             self.assertIn('<h1>Color Form</h1>', html)
+
+    def test_color_form(self):
+        with app.test_client() as client:
+            res = client.post('/fav-color', data={'color': 'orange'})
+            html = res.get_data(as_text=True)
+
+            self.assertEqual(res.status_code, 200)
+            self.assertIn('<h3>Woah! I like orange, too</h3>', html)
