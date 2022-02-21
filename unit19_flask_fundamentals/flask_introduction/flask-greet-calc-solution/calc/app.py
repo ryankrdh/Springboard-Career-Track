c2d2f1b8c5d3052d0fc89138ebb96614b1cc4b78
ryankrdh <ryankrdh@gmail.com>
2022-02-20 22:48:43 -0800

    notes

--- unit19_flask_fundamentals/flask_introduction/flask-greet-calc-solution/calc/app.py
+++ unit19_flask_fundamentals/flask_introduction/flask-greet-calc-solution/calc/app.py
@@ -62,3 +62,18 @@ def do_math(oper):
 
     return str(result)
 
+
+'''
+types of flask requests:
+
+request.args: the key/value pairs in the URL query string
+
+request.form: the key/value pairs in the body, from a HTML post form, or JavaScript request that isn't JSON encoded
+
+request.files: the files in the body, which Flask keeps separate from form. HTML forms must use enctype=multipart/form-data or files will not be uploaded.
+
+request.values: combined args and form, preferring args if keys overlap
+
+request.json: parsed JSON data. The request must have the application/json content type, or use request.get_json(force=True) to ignore the content type.
+
+'''
